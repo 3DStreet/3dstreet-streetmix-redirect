4f31438b80b2197809429426dc9127992cde0485
Kieran Farr <kieran.farr@gmail.com>
2023-01-31 20:03:19 -0800

    forward to 3dstreet.app
    instead of github.3dstreet.org

--- functions/index.js
+++ functions/index.js
@@ -3,5 +3,5 @@ const functions = require("firebase-functions");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 exports.streetmixRedirect = functions.https.onRequest((req, res) => {
-  res.redirect("https://github.3dstreet.org/#https://streetmix.net" + req.path);
+  res.redirect("https://3dstreet.app/#https://streetmix.net" + req.path);
 });
